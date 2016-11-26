55ec760a59ca1d6682a8c6244fe92051d4a2341e
oldercai <112735239@qq.com>
2016-11-26 14:24:36 +0800

    上传data文件夹

---
+++ src/first/data/config.php
@@ -0,0 +1,38 @@
+<?php
+// database host
+$db_host   = "127.0.0.1:3306";
+
+// database name
+$db_name   = "cnpicks";
+
+// database username
+$db_user   = "cnpicks";
+
+// database password
+$db_pass   = "wangyabener";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+if(!defined('ADMIN_PATH'))
+{
+define('ADMIN_PATH','admin');
+}
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2016-11-25 09:41:59');
+
+?>
